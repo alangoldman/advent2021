9c30bde958fbeee08f04367cc5c3302ab60dd75e
Alan Goldman <alan.goldman@gatech.edu>
2021-12-28 23:56:43 -0500

    Day 1

---
+++ day1.py
@@ -0,0 +1,29 @@
+file = open("day1_input.txt", "r")
+lines = file.readlines()
+file.close()
+
+data = [int(l) for l in lines]
+
+increased = 0
+last = None
+for depth in data:
+    if last is not None and depth > last:
+        increased += 1
+    last = depth
+
+print(increased)
+
+#part 2
+
+increased = 0
+window = []
+windows = []
+last = None
+for depth in data:
+    window.append(depth)
+    if len(window) < 3:    
+        continue
+    windows.append(sum(window))
+    window.pop(0)
+
+print(increased)
