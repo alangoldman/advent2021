ffd98f04301629dc26eb8e3ba7c2777147ad04f6
Alan Goldman <alan.goldman@gatech.edu>
2021-12-29 01:31:02 -0500

    Day 6b

--- day6.py
+++ day6.py
@@ -3,12 +3,18 @@ file = open("day6_input.txt", "r")
 lines = file.readlines()
 file.close()
 
-fish = np.array([int(l.strip()) for l in lines[0].split(',')])
-#fish = np.array([3,4,3,1,2]) # example
- 
+fish_n = np.array([int(l.strip()) for l in lines[0].split(',')])
+#fish_n = np.array([3,4,3,1,2]) # example
+
+fish = {}
+for i in range(11):
+    fish[i] = len(fish_n[fish_n==i])
+
 for day in range(256):
-    fish = np.append(fish, np.ones((len(fish[fish==0])))*9)
-    fish[fish==0] = 7
-    fish -= 1
-    #print(day+1, len(fish), fish)
-print(len(fish))
+    fish[9] += fish[0]
+    fish[7] += fish[0]
+    fish[0] = 0
+    for i in range(10):
+        fish[i] = fish[i+1]
+    #print(day+1, sum(fish.values()), fish)
+print(sum(fish.values()))
